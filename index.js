7ffb2064e4deee92bfce6249b3b9668d850a343d
“your <keerthigaputtur21@gmail.com>
2021-10-02 11:35:50 +0530

    getUserInfo & readDrive

--- index.js
+++ index.js
@@ -48,6 +48,42 @@ oAuth2Client.getToken(req.body.code, (err,token)=>{
 });
 });
 
+app.post('/getUserInfo', (req, res) => {
+  if (req.body.token == null) return res.status(400).send('Token not found');
+  oAuth2Client.setCredentials(req.body.token);
+  const oauth2 = google.oauth2({ version: 'v2', auth: oAuth2Client });
+
+  oauth2.userinfo.get((err, response) => {
+      if (err) res.status(400).send(err);
+      console.log(response.data);
+      res.send(response.data);
+  })
+});
+
+app.post('/readDrive', (req, res) => {
+  if (req.body.token == null) return res.status(400).send('Token not found');
+  oAuth2Client.setCredentials(req.body.token);
+  const drive = google.drive({ version: 'v3', auth: oAuth2Client });
+  drive.files.list({
+      pageSize: 10,
+  }, (err, response) => {
+      if (err) {
+          console.log('The API returned an error: ' + err);
+          return res.status(400).send(err);
+      }
+      const files = response.data.files;
+      if (files.length) {
+          console.log('Files:');
+          files.map((file) => {
+              console.log(`${file.name} (${file.id})`);
+          });
+      } else {
+          console.log('No files found.');
+      }
+      res.send(files);
+  });
+});
+
 
   
 app.listen(port, () => {
